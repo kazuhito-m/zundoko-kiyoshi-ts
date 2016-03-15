2ac39ada274b32af0552dbe1afda99b0eb6dd457
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-03-15 19:29:52 +0900

    テストファーストで文字列変換処理を定義(現在こける。)

--- src/main/ZundokoKiyoshi.ts
+++ src/main/ZundokoKiyoshi.ts
@@ -2,18 +2,55 @@
 
 export default class ZundokoKiyoshi {
     
-    public ZUN:boolean  = false;
-    public DOKO:boolean = true;
+    // "ズン"と"ドコ"の定数
+    public static ZUN:boolean  = false;
+    public static DOKO:boolean = true;
     
     public constructor() {
     }
     
+    // ズンドコ列を真偽値の配列で返す。
     public getZundokoPeriod():boolean[] {
-        // 気に入らないが…ローカルで短く書くには再定義しかない。        
-        const ZUN:boolean = this.ZUN;
-        const DOKO:boolean = this.DOKO;
         
-        let t:boolean[] = [ZUN,ZUN,ZUN,ZUN,DOKO];
-        return t;
-    } 
+        let zndkLog:boolean[] = []; // ズンドコの履歴
+        let zunCount:number = 0;    // ズンカウンタ
+        let zunOrDoko:boolean = ZundokoKiyoshi.DOKO;   // カレントの値
+
+        // ズン４つに最後がドコになるまで。
+        while (!(zunCount >= 4 && zunOrDoko == ZundokoKiyoshi.DOKO)) {
+            // カレントが"ズン"ならカウントアップ、"ドコ"なら初期化         
+            zunCount++;
+            if (zunOrDoko == ZundokoKiyoshi.DOKO) zunCount = 0;
+            // ズンドコ取得
+            zunOrDoko = this.genZunDoko();
+            // リストには絶えず足す。
+            zndkLog.push(zunOrDoko);
+        }
+        
+        return zndkLog;
+    }
+    
+    // ズンorドコをランダムに返す。
+    public genZunDoko():boolean {
+        return (Math.random() * 2) < 1;
+    }
+    
+    // 文字列をコンソールに書く。
+    public execZundoko() {
+        
+    }
+    
+    public convertZundokoString(zndkLog:boolean[]) {
+        let printer =  new ZundokoPrinter();
+        return printer.makeZundokoString(zndkLog);
+    }
+    
+}
+
+// ズンドコ文字列の生成だけを責務にしたクラス。
+class ZundokoPrinter {
+    // ズンドコ配列を文字列に変換する。
+    public makeZundokoString(zndkLog:boolean[]) {
+        return "";  //　仮実装   
+    }
 }
--- src/test/ZundokoKiyoshi.test.ts
+++ src/test/ZundokoKiyoshi.test.ts
@@ -5,16 +5,37 @@ import ZundokoKiyoshi from '../main/ZundokoKiyoshi';
 
 describe("ZundokoKiyoshi", () => {
 
-    var zndk = new ZundokoKiyoshi();
+    var sut = new ZundokoKiyoshi();
 
-	it("ズンドコの概念を定数として提供するか", () => {
-		assert.equal(zndk.ZUN, false);
-		assert.equal(zndk.DOKO, true);
-	});
-    
+    it("ズンドコの概念を定数として提供するか", () => {
+        assert.equal(ZundokoKiyoshi.ZUN, false);
+        assert.equal(ZundokoKiyoshi.DOKO, true);
+    });
+
     it("ズンドコの配列を取得できるか" , () => {
-        let actual:boolean[] = zndk.getZundokoPeriod();
+        let actual:boolean[] = sut.getZundokoPeriod();
+        // console.log("カウントは " + actual.length);
+        // console.log(actual.join(","));
+        let zndk:string = "";
         assert.equal(actual.length > 4, true);
     });
+
+    it("ズンドコのランダマイズはある程度出来ているか", () => {
+        var zc:number = 0;
+        for (var i = 0;i < 10000;i++) if (sut.genZunDoko()) zc++;
+        assert.equal(zc > 4750, true);
+        assert.equal(zc < 5200, true);
+    });
+
+    it("ズンドコの文字列表現への変換は可能か", () => {
+        // 気に入らないが…ローカルで短く書くには再定義しかない。        
+        const ZUN:boolean = ZundokoKiyoshi.ZUN;
+        const DOKO:boolean = ZundokoKiyoshi.DOKO;
+        
+        let actual:string = sut.convertZundokoString([ZUN,DOKO,ZUN,ZUN,ZUN,ZUN,DOKO]);
+
+        assert.equal(actual, "ズンドコズンズンズンズンドコ");
+    });
+    
 });
 
