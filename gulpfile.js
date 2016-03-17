8c3a07f0d008a8d1817eaa6760a9a4f154bc1b17
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-03-18 02:35:04 +0900

    まずは「JSだけでカバレッジを取れる」までは変更。

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gutil = require('gulp-util');
 var mocha = require('gulp-mocha');
 var sourcemaps = require('gulp-sourcemaps');
 // var concat = require('gulp-concat');
+var istanbul = require('gulp-istanbul');
 
 // 定数群
 const TEST_BUILD_DIR = './test-build/';
@@ -30,4 +31,20 @@ gulp.task('test-mocha', function() {
   return gulp.src([TEST_BUILD_DIR + 'test/*.test.js'], { read: false })
     .pipe(mocha({ reporter: 'list'}))
     .on('error', gutil.log);
+});
+
+gulp.task('pre-test', function () {
+  return gulp.src([TEST_BUILD_DIR + 'main/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+
+gulp.task('test', ['test-transpile','pre-test'] , function() {
+  return gulp.src([TEST_BUILD_DIR + 'test/*.test.js'], { read: false })
+    .pipe(mocha({ reporter: 'list'}))
+    .on('error', gutil.log)
+    .pipe(istanbul.writeReports())
+    // アウトの基準は"75%"くらいにしとく？
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 75 } }));
 });
