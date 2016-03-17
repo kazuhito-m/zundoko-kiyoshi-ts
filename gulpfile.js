f7b027a39e85e67bf471f348ded75ef664c25bdb
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-03-18 02:35:58 +0900

    微調整。書き方で整合性が取れていないところを修正。

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,17 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var mocha = require('gulp-mocha');
 var sourcemaps = require('gulp-sourcemaps');
-// var concat = require('gulp-concat');
 var istanbul = require('gulp-istanbul');
-
-// 定数群
-const TEST_BUILD_DIR = './test-build/';
+// var concat = require('gulp-concat');
 
 // typescriptのオブジェクトと、tsconfig.jsonを読み込んだプロジェクトオブジェクト作成。 
 var typescript = require('gulp-typescript');
 var tsProject = typescript.createProject('tsconfig.json', function() {
   typescript: require('typescript')
 });
+
+// 定数群
+const TEST_BUILD_DIR = './test-build/';
 
 gulp.task('test-transpile', function() {
   // 対象となるファイルを全部指定
@@ -25,8 +25,6 @@ gulp.task('test-transpile', function() {
     .pipe(gulp.dest(TEST_BUILD_DIR));
 });
 
-
-
 gulp.task('test-mocha', function() {
   return gulp.src([TEST_BUILD_DIR + 'test/*.test.js'], { read: false })
     .pipe(mocha({ reporter: 'list'}))
@@ -38,7 +36,6 @@ gulp.task('pre-test', function () {
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
-
 
 gulp.task('test', ['test-transpile','pre-test'] , function() {
   return gulp.src([TEST_BUILD_DIR + 'test/*.test.js'], { read: false })
