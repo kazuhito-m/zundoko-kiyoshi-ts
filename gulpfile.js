e545f29af8c59332745cc7501a1a15202838fcc1
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-03-18 01:26:31 +0900

    少し手直し。気になるところをFIX。

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var mocha = require('gulp-mocha');
+var sourcemaps = require('gulp-sourcemaps');
 // var concat = require('gulp-concat');
 
 // 定数群
-var TEST_BUILD_DIR = './test-build/';
+const TEST_BUILD_DIR = './test-build/';
 
 // typescriptのオブジェクトと、tsconfig.jsonを読み込んだプロジェクトオブジェクト作成。 
 var typescript = require('gulp-typescript');
@@ -14,12 +15,10 @@ var tsProject = typescript.createProject('tsconfig.json', function() {
 
 gulp.task('test-transpile', function() {
   // 対象となるファイルを全部指定
-  gulp.src(['./src/main/*.ts','./src/test/*.ts'])
+  gulp.src(['./src/**/*.ts','!./src/typings/**'])
     .pipe(typescript(tsProject))
     // jsプロパティを参照
-    .js 
-    // ファイルをひとまとめに
-    // .pipe(concat("main.js"))
+    .js
     .pipe(gulp.dest(TEST_BUILD_DIR));
 });
 
