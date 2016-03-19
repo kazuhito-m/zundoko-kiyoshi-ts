a5b2518d2254aab6cf3dbf6c06a3671167bb7d9a
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-03-20 02:01:06 +0900

    Update 2016-03-19T17:01:02.285Z

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 "use strict";
 class AppVersion {
 }
-AppVersion.version = "1.0.1";
+AppVersion.version = "1.0.4";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = AppVersion;
 
