0ef34459f2ea63a0641267ea6f867fb197ac769f
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-03-20 01:19:32 +0900

    リファクタリング。プログラム以外にもコードフォーマットを書ける。

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,9 @@ var typescript = require('gulp-typescript');
 var ghPages = require('gulp-gh-pages');
 var bump = require('gulp-bump');
 var fs = require('fs');
-var through  = require('through2');
+var through = require('through2');
 var git = require('gulp-git');
-var tsProject = typescript.createProject('tsconfig.json', function() {
+var tsProject = typescript.createProject('tsconfig.json', function () {
     // typescriptのオブジェクトと、tsconfig.jsonを読み込んだプロジェクトオブジェクト作成。
     typescript: require('typescript')
 });
@@ -24,13 +24,13 @@ const TEST_BUILD_DIR = './test-build/';
 
 // タスク群。
 
-gulp.task('test-clean', function(cb) {
+gulp.task('test-clean', function (cb) {
     return del([TEST_BUILD_DIR, './coverage'], cb);
 });
 
-gulp.task('test-src-copy' , ['test-clean'] , function() {
-    return gulp.src([ './src/**' ])
-        .pipe( gulp.dest(TEST_BUILD_DIR) );
+gulp.task('test-src-copy', ['test-clean'], function () {
+    return gulp.src(['./src/**'])
+        .pipe(gulp.dest(TEST_BUILD_DIR));
 });
 
 // FIXME 以下、苦肉の策で「sourcemap取るために二回コンパイルして」いる。こんなの絶対おかしいからなおすこと。
@@ -47,71 +47,71 @@ gulp.task('test-src-copy' , ['test-clean'] , function() {
 //         .pipe(gulp.dest(TEST_BUILD_DIR));
 // });
 
-gulp.task('test-transpile' , ['test-src-copy'], function() {
+gulp.task('test-transpile', ['test-src-copy'], function () {
     // 対象となるファイルを全部指定
-    return gulp.src([TEST_BUILD_DIR + '**/*.ts','!' + TEST_BUILD_DIR + '/typings/**'] )
+    return gulp.src([TEST_BUILD_DIR + '**/*.ts', '!' + TEST_BUILD_DIR + '/typings/**'])
         .pipe(typescript(tsProject))
-        // jsプロパティを参照
+    // jsプロパティを参照
         .js
         .pipe(gulp.dest(TEST_BUILD_DIR));
 });
 
-gulp.task('test-retranspile-main' , ['test-transpile'], function() {
+gulp.task('test-retranspile-main', ['test-transpile'], function () {
     // 対象となるファイルを全部指定
-    return gulp.src([TEST_BUILD_DIR + 'main/*.ts'] , {base:TEST_BUILD_DIR + 'main/'} )
+    return gulp.src([TEST_BUILD_DIR + 'main/*.ts'], { base: TEST_BUILD_DIR + 'main/' })
         .pipe(sourcemaps.init())
         .pipe(typescript(tsProject))
-        // jsプロパティを参照
+    // jsプロパティを参照
         .js
-        // ちょいカスタム。ソースのありか（のトップ）を指定。
-        .pipe(sourcemaps.write('./' , {includeContent: false, sourceRoot: ''}))
+    // ちょいカスタム。ソースのありか（のトップ）を指定。
+        .pipe(sourcemaps.write('./', { includeContent: false, sourceRoot: '' }))
         .pipe(gulp.dest(TEST_BUILD_DIR + 'main/'));
 });
 
-gulp.task('pre-test', ['test-retranspile-main'] , function () {
-  return gulp.src([TEST_BUILD_DIR + 'main/*.js'])
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
+gulp.task('pre-test', ['test-retranspile-main'], function () {
+    return gulp.src([TEST_BUILD_DIR + 'main/*.js'])
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['pre-test'] , function() {
-  return gulp.src([TEST_BUILD_DIR + 'test/*Test.js'], { read: false })
-    .pipe(mocha({ reporter: 'list'}))
-    .on('error', gutil.log)
-    .pipe(istanbul.writeReports())
-    .on('end', function() {
-        // remap-istanbul(TypeScrptへのカバレッジの付け替え)
-        return gulp.src('./coverage/coverage-final.json')
-            .pipe(remapIstanbul({
-                reports: {
-                    'json': './coverage/coverage.json',
-                    'html': './coverage/html-report'
-                }
-            }));
-    })
+gulp.task('test', ['pre-test'], function () {
+    return gulp.src([TEST_BUILD_DIR + 'test/*Test.js'], { read: false })
+        .pipe(mocha({ reporter: 'list' }))
+        .on('error', gutil.log)
+        .pipe(istanbul.writeReports())
+        .on('end', function () {
+            // remap-istanbul(TypeScrptへのカバレッジの付け替え)
+            return gulp.src('./coverage/coverage-final.json')
+                .pipe(remapIstanbul({
+                    reports: {
+                        'json': './coverage/coverage.json',
+                        'html': './coverage/html-report'
+                    }
+                }));
+        })
     // アウトの基準は"75%"くらいにしとく？
-    .pipe(istanbul.enforceThresholds({ thresholds: { global: 75 } }));
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: 75 } }));
 });
 
 // patchバージョンを上げる
-gulp.task('verup-patch', function(){
+gulp.task('verup-patch', function () {
     gulp.src('./package.json')
-        .pipe(bump({type:'patch'}))
-        .pipe(through.obj(function(file,enc,cb) {
+        .pipe(bump({ type: 'patch' }))
+        .pipe(through.obj(function (file, enc, cb) {
             // package.json からファイルを読んで、version取り出し、クラスファイルに反映。
             var packageJson = JSON.parse(file._contents);
             var code = 'export default class AppVersion {\n	public static version = "' + packageJson.version + '";\n}';
             fs.writeFile('./src/main/AppVersion.ts', code);
             cb(null, file);
         }))
-        .on('end', function() {
+        .on('end', function () {
             // 成功で終わったら、書き換えたファイルをcommit するように。
-            return gulp.src(['./package.json','./src/main/AppVersion.ts'])
+            return gulp.src(['./package.json', './src/main/AppVersion.ts'])
                 .pipe(git.commit('patch version incliment.'));
         })
         .pipe(gulp.dest('./'));
 });
- 
+
 gulp.task('build', function () {
     return browserify()
         .add('./src/main/ZundokoKiyoshi.ts')
@@ -125,10 +125,10 @@ gulp.task('build', function () {
         .pipe(gulp.dest('./site/js'));
 });
 
-gulp.task('deploy', ['verup-patch'], function() {
+gulp.task('deploy', ['verup-patch'], function () {
     return gulp.src('./site/**/*')
         .pipe(ghPages())
-        .on('end', function() {
+        .on('end', function () {
             // Version番号をインクリメントしているので、git pushをしておく
             // (色々あったら問題だがgit的にはクリーンな状態でdeployしているだろうという推測)
             return git.push();
