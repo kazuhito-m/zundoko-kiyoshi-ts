910ae7ecf3d12c30d969a3e521c8466e1c8b8c6d
wercker <wercker@example.com>
2016-11-16 14:29:31 +0000

    Update 2016-11-16T14:29:30.500Z

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 "use strict";
 class AppVersion {
 }
-AppVersion.version = "1.0.12";
+AppVersion.version = "1.0.13";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = AppVersion;
 
